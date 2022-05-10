fc01e000b3901b7a0924bee527f97adbcadc24f9
lyalka-qa <ppasd2005@yandex.ru>
2022-05-10 19:06:11 +0300

    Initial commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,74 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldCalcSumSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+
+        int actual = service.calcSumSales(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcAverageSumSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+
+        int actual = service.calcAverageSumSales(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+
+        int actual = service.maxSales(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcMinSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        int actual = service.minSales(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcAmountOfMonthSalesLowerAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.amountOfMonthSalesLowerAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcAmountOfMonthSalesHigherAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.amountOfMonthSalesHigherAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+}
